0ef250eec8992bed2450869fdb9f1f1794764d0b
Lukáš Cvoliga <lukascvoliga2005@gmail.com>
2026-09-22 12:24:12 +0200

    uloha

--- Uloha1a/Uloha1a.sql
+++ Uloha1a/Uloha1a.sql
@@ -1,18 +1,20 @@
-Create database superstore;
-Create table customers(
+CREATE DATABASE superstore;
+CREATE TABLE customers (
     customer_id VARCHAR(20) PRIMARY KEY,
     customer_name VARCHAR(100),
     segment VARCHAR(50),
     country VARCHAR(50),
     region VARCHAR(50)
-)
-Create table products(
+);
+
+CREATE TABLE products (
     product_id VARCHAR(20) PRIMARY KEY,
     category VARCHAR(50),
     sub_category VARCHAR(50),
     product_name VARCHAR(100)
-)
-Create table orders(
+);
+
+CREATE TABLE orders (
     order_id VARCHAR(20) PRIMARY KEY,
     customer_id VARCHAR(20) NOT NULL,
     product_id VARCHAR(20) NOT NULL,
@@ -24,7 +26,8 @@ Create table orders(
     profit NUMERIC(10,2) NOT NULL,
     
     FOREIGN KEY (customer_id)
-        REFERENCES customers(customer_id)
+        REFERENCES customers(customer_id),
+    
     FOREIGN KEY (product_id)
         REFERENCES products(product_id)
-)
+);
